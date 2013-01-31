9dc7cbc2e5aaabd2d02badaf2ad4f20fd23dca92
Jonathan Brinley <jbrinley@adeliedesign.com>
2013-01-31 15:29:24 -0500

    test label decorator

---
+++ decorators/WP_Form_Decorator_Label_Test.php
@@ -0,0 +1,34 @@
+<?php
+
+class WP_Form_Decorator_Label_Test extends WP_UnitTestCase {
+	public function test_label() {
+		$element = WP_Form_Element::create('text')
+			->set_label( 'This is my label' )
+			->set_view( new WP_Form_View_Text() )
+			->add_decorator( 'WP_Form_Decorator_Label' );
+		$output = $element->render();
+
+		$this->assertRegExp('!^<label[^>]*>This is my label</label>.*<[^>]*>!', $output);
+	}
+
+	public function test_class() {
+		$element = WP_Form_Element::create('text')
+			->set_label( 'This is my label' )
+			->set_view( new WP_Form_View_Text() )
+			->add_decorator( 'WP_Form_Decorator_Label' );
+		$output = $element->render();
+
+		$this->assertRegExp('!<label[^>]*class="form-label"[^>]*>!', $output);
+	}
+
+	public function test_for() {
+		$element = WP_Form_Element::create('text')
+			->set_label( 'This is my label' )
+			->set_view( new WP_Form_View_Text() )
+			->add_decorator( 'WP_Form_Decorator_Label' )
+			->set_attribute('id', 'my-form-element');
+		$output = $element->render();
+
+		$this->assertRegExp('!<label[^>]*for="my-form-element"[^>]*>!', $output);
+	}
+}
