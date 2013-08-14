d3f90116e93a8f846f3fb0ee00f9be1717066e2b
Jonathan Brinley <jbrinley@flightless.us>
2013-08-14 17:14:54 -0400

    allow setting of select WP contants via environment vars

--- bootstrap.php
+++ bootstrap.php
@@ -8,6 +8,13 @@ $GLOBALS['wp_tests_options'] = array(
 	'stylesheet' => 'twentytwelve',
 );
 
+// Check for select constants defined as environment variables
+foreach ( array('WP_CONTENT_DIR', 'WP_CONTENT_URL', 'WP_PLUGIN_DIR', 'WP_PLUGIN_URL', 'WPMU_PLUGIN_DIR') as $env_constant ) {
+	if ( false !== getenv( $env_constant ) && !defined( $env_constant ) ) {
+		define( $env_constant, getenv( $env_constant ));
+	}
+}
+
 // If the wordpress-tests repo location has been customized (and specified
 // with WP_TESTS_DIR), use that location. This will most commonly be the case
 // when configured for use with Travis CI.
