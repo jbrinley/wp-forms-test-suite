ec8294da8c2008c82d92c7b5da8003476c89aeb3
Jonathan Brinley <jbrinley@flightless.us>
2013-08-27 10:52:52 -0400

    test forms with arguments

--- test_cases/forms/WP_Form_Registrar_Test.php
+++ test_cases/forms/WP_Form_Registrar_Test.php
@@ -31,6 +31,25 @@ class WP_Form_Registrar_Test extends WP_UnitTestCase {
 		$this->registrar->get_form( 'test-form-one' );
 	}
 
+	public function test_arguments() {
+		$this->registrar->register( 'test-form-two', array( $this, '_test_form_two_callback' ) );
+		$form = $this->registrar->get_form( 'test-form-two', 'Given Name', 'Family Name' );
+		$this->assertEquals( 'Given Name', $form->get_element('first_name')->get_label() );
+		$this->assertEquals( 'Family Name', $form->get_element('last_name')->get_label() );
+	}
+
+	public function test_varying_arguments() {
+		$this->registrar->register( 'test-form-two', array( $this, '_test_form_two_callback' ) );
+
+		$form = $this->registrar->get_form( 'test-form-two', 'Given Name', 'Family Name' );
+		$this->assertEquals( 'Given Name', $form->get_element('first_name')->get_label() );
+		$this->assertEquals( 'Family Name', $form->get_element('last_name')->get_label() );
+
+		$form = $this->registrar->get_form( 'test-form-two', 'Name One', 'Name Two' );
+		$this->assertEquals( 'Name One', $form->get_element('first_name')->get_label() );
+		$this->assertEquals( 'Name Two', $form->get_element('last_name')->get_label() );
+	}
+
 	public function test_invalid_form() {
 		$this->setExpectedException('InvalidArgumentException');
 		$this->registrar->get_form( 'test-form-one' );
@@ -53,6 +72,20 @@ class WP_Form_Registrar_Test extends WP_UnitTestCase {
 				WP_form_Element::create('text')
 					->set_name('last_name')
 					->set_label('Last Name')
+			);
+	}
+
+	public function _test_form_two_callback( WP_Form $form, $first_name_label = "First Name", $last_name_lael = "Last Name" ) {
+		$form
+			->add_element(
+				WP_Form_Element::create('text')
+					->set_name('first_name')
+					->set_label($first_name_label)
+			)
+			->add_element(
+				WP_form_Element::create('text')
+					->set_name('last_name')
+					->set_label($last_name_lael)
 			);
 	}
 }
